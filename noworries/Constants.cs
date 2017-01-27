6240f2556538d03fdb24c054aa0e2744e158b93e
김은지 <eunji@gim-eunjiui-MacBook-Air.local>
2017-01-27 12:48:22 +0900

    initialize project

---
+++ noworries/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace noworries
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://noworries.azurewebsites.net";
+	}
+}
+
---
+++ noworries/RestaurantList.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace noworries
+{
+	public partial class RestaurantList : ContentPage
+	{
+		
+
+		public RestaurantList()
+		{
+			InitializeComponent();
+
+
+		}
+
+		//public async void OnSyncItems(object sender, EventArgs e)
+		//{
+		//	await RefreshItems(true, true);
+		//}
+	}
+}
